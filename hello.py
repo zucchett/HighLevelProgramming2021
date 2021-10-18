6e3a851c29925642f7caf159d659600b434ac42a
ThomasGicquel <gicquelthomas.tg@gmail.com>
2021-10-18 12:43:27 +0200

    ex1

--- hello.py
+++ hello.py
@@ -5,4 +5,14 @@ Created on Fri Oct  8 11:15:56 2021
 @author: User
 """
 
-print("hello high level programming")
+tup=(0,2,10,6)
+
+def normalized(tup):
+    tup_normal=()
+    m=max(tup)
+    for i in tup:
+        tup_normal+=(i/m,)
+    return tup_normal
+    
+print(normalized(tup))
+
