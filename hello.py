9040bf5e778d9f9d06744de6222f2316164b5295
ThomasGicquel <gicquelthomas.tg@gmail.com>
2021-10-22 10:42:01 +0200

    cour 2

--- hello.py
+++ hello.py
@@ -1,18 +1,7 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Oct  8 11:15:56 2021
+Created on Fri Oct 22 10:40:40 2021
 
 @author: User
 """
-
-tup=(0,2,10,6)
-
-def normalized(tup):
-    tup_normal=()
-    m=max(tup)
-    for i in tup:
-        tup_normal+=(i/m,)
-    return tup_normal
-    
-print(normalized(tup))
-
+print("hello world")
